2de371d2772bb404a32da799eb8f43bde13008b2
simondole <simondole@192.168.31.66>
2015-12-15 17:03:58 +0100

    f

--- src/ProfDisplay.java
+++ src/ProfDisplay.java
@@ -1,14 +1,15 @@
 import javax.swing.*;
+import java.awt.* ;
 
 
 public class ProfDisplay extends JFrame{
 	
 	public ProfDisplay(){
-	super("Outil Professeur");
-	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    setLocation(0,200);
-    setSize(1000,500);
-    setVisible(true);
+		super("Outil Professeur");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocation(0,200);
+		setSize(1000,500);
+		setVisible(true);
 	}
 
 }
